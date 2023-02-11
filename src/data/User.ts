a321d08952f85a5dab1f4c02b52f7dcfbce3519e
Rob Coenen <robcvdv@gmail.com>
2023-02-11 02:44:02 +0100

    - some more care to ensure types and utils go in root project and postinstall copy script works for client sub-project

--- src/data/User.ts
+++
@@ -1,7 +0,0 @@
-import { Id } from "../types/Id";
-
-export type User = {
-  id: Id;
-  socketId: Id;
-  name: string;
-};
--- src/utils/types/RequireSome.ts
+++
@@ -1,1 +0,0 @@
-export type RequireSome<T, TR extends keyof T> = T & Required<Pick<T, TR>>;
